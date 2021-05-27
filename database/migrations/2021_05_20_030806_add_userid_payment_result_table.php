522939feb7f2f06f3b8c26a283d377b8ee2d8c5b
Jemaica Mae Mon <201810138@gordoncollege.edu.ph>
2021-05-27 04:20:41 -0700

    Fixed Bugs

--- database/migrations/2021_05_20_030806_add_userid_payment_result_table.php
+++ database/migrations/2021_05_20_030806_add_userid_payment_result_table.php
@@ -14,7 +14,7 @@ class AddUseridPaymentResultTable extends Migration
     public function up()
     {
         Schema::table('payment_results', function (Blueprint $table) {
-            $table->bigInteger('user_id')->unsigned();
+            // $table->bigInteger('user_id')->unsigned();
         });
     }
 
